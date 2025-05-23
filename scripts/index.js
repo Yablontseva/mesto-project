6ecd803691fff03feb92233d6ee3c40f41a9bcf2
Anastasia Yablontseva <nastyayabl02802@gmail.com>
2025-05-23 22:20:16 +0300

    Базовая отрисовка карточек

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,61 @@
 
 // @todo: DOM узлы
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const placesList = document.querySelector('.places__list');
+
+// Функция создания карточки
+function createCard(cardData) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  
+  return cardElement;
+}
+
+// Отрисовка карточек
+function renderCards() {
+  initialCards.forEach(card => {
+    placesList.append(createCard(card));
+  });
+}
+
+// Инициализация
+document.addEventListener('DOMContentLoaded', renderCards);
+
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
 
-// ====== Константы и DOM-элементы ======
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  // ... остальные 5 карточек
-];
-
-// ====== Функции для карточек ======
-function createCard(cardData) {
-  // Реализуйте создание карточки
-}
-
-function renderCards() {
-  // Отрисуйте карточки на странице
-}
-
-// ====== Инициализация ======
-document.addEventListener('DOMContentLoaded', () => {
-  renderCards();
-});
