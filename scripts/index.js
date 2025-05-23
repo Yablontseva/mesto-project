769b656e63fccad16e1f0c92af0dc2e73df39341
Anastasia Yablontseva <nastyayabl02802@gmail.com>
2025-05-23 21:57:43 +0300

    Реализовано: базовый функционал карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+// ====== Константы и DOM-элементы ======
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  // ... остальные 5 карточек
+];
+
+// ====== Функции для карточек ======
+function createCard(cardData) {
+  // Реализуйте создание карточки
+}
+
+function renderCards() {
+  // Отрисуйте карточки на странице
+}
+
+// ====== Инициализация ======
+document.addEventListener('DOMContentLoaded', () => {
+  renderCards();
+});
